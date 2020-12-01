f7509dccbdb5b18d8cd53914438ec6a1bdf9c58b
F R Summit <www.frsummit1@gmail.com>
2020-12-01 10:59:29 +0600

    single product page dynamic gallery done

--- wp-content/themes/frskynet/woocommerce/single-product/product-thumbnails.php
+++ wp-content/themes/frskynet/woocommerce/single-product/product-thumbnails.php
@@ -45,15 +45,42 @@ if($attachment_ids) {
 	$loop    = 0;
 	$columns = apply_filters('woocommerce_product_thumbnails_columns', 3);
 	?>
-	<div class="thumbnails <?php echo 'columns-' . $columns; ?>"></div>
-	<?php
+	<div id="owl-single-product-thumbnails"><?php
 		foreach( $attachment_ids as $attachment_id ) {
 			$classes  = array( 'zoom' );
 
 			if($loop === 0 || $loop % $columns === 0) {
 				$classes[] = 'first';
 			}
-
 			
+			if( ( $loop + 1 ) % $columns === 0) {
+				$classes[] = 'last';
+			}
+
+			$image_class = implode(' ', $classes);
+			$props       = wc_get_product_attachment_props( $attachment_id, $post );
+
+			if( !$props['url'] ) {
+				continue;
+			}
+
+			echo apply_filters(
+				'woocommerce_single_product_image_thumbnail_html',
+				sprintf(
+					'<a href="%s" class="%s" title="%s" data-rel="prettyPhoto[product-gallery]">%s</a>',
+					esc_url( $props['url'] ),
+					esc_attr( $image_class ),
+					esc_attr( $props['caption'] ),
+					wp_get_attachment_image( $attachment_id, apply_filters(
+						'single_product_small_thumbnail_size', 'shop_thumbnail'), 0, $props
+					)
+				),
+				$attachment_id,
+				$post->ID,
+				esc_attr( $image_class )
+			);
+			$loop++;
 		}
+	?></div>
+	<?php
 }
